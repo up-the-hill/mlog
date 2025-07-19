b9c8292b36e2dd3c2731f096d605490300c220d6
up-the-hill <aj.sth.2022@gmail.com>
2025-07-19 12:44:08 +1000

    MVP

--- main.go
+++ main.go
@@ -10,39 +10,7 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type State int
-
 const BASE_PATH = "/tmp/"
-
-const (
-	StateStart State = iota
-	StateBook
-	StateMovie
-	StateQuote
-	StateMusing
-)
-
-var stateNames = map[int]string{
-	1: "book",
-	2: "movie",
-	3: "quote",
-	4: "musing",
-}
-
-type book struct {
-	Name string
-	Date time.Time
-}
-
-type movie struct {
-	Name string
-	Date time.Time
-}
-
-type quote struct {
-	Quote string
-	Date  time.Time
-}
 
 type musing struct {
 	Musing string
@@ -58,9 +26,6 @@ func appendEntry(filename string, newEntry any) {
 }
 
 type model struct {
-	cursor    int
-	choices   []string
-	state     int
 	textinput textinput.Model
 }
 
@@ -70,8 +35,6 @@ func initialModel() model {
 	ti.CharLimit = 156
 	ti.Width = 20
 	return model{
-		choices:   []string{"Book", "Movie", "Quote", "Thoughts/Musings"},
-		state:     int(StateStart),
 		textinput: ti,
 	}
 }
@@ -82,66 +45,23 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
-	switch m.state {
-	case int(StateStart):
-		switch msg := msg.(type) {
-		case tea.KeyMsg:
-			switch msg.String() {
-			case "ctrl+c", "q":
-				return m, tea.Quit
-			case "up", "k":
-				if m.cursor > 0 {
-					m.cursor--
-				}
-			case "down", "j":
-				if m.cursor < len(m.choices)-1 {
-					m.cursor++
-				}
-			case "enter", " ":
-				m.state = m.cursor + 1
-			}
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+
+		case "enter":
+			appendEntry(BASE_PATH+"musings.ndjson", &musing{
+				Musing: m.textinput.Value(),
+				Date:   time.Now(),
+			},
+			)
+
+			return m, tea.Quit
 		}
-	default:
-		switch msg := msg.(type) {
-		case tea.KeyMsg:
-			switch msg.String() {
-			case "ctrl+c":
-				return m, tea.Quit
-			case "enter":
-				// select file to write to
-				f := BASE_PATH + stateNames[m.state] + "s.ndjson"
-				// format output
-				switch m.state {
-				case int(StateBook):
-					res := &book{
-						Name: m.textinput.Value(),
-						Date: time.Now(),
-					}
-					appendEntry(f, res)
-				case int(StateMovie):
-					res := &movie{
-						Name: m.textinput.Value(),
-						Date: time.Now(),
-					}
-					appendEntry(f, res)
-				case int(StateQuote):
-					res := &quote{
-						Quote: m.textinput.Value(),
-						Date:  time.Now(),
-					}
-					appendEntry(f, res)
-				case int(StateMusing):
-					res := &musing{
-						Musing: m.textinput.Value(),
-						Date:   time.Now(),
-					}
-					appendEntry(f, res)
-				}
-				return m, tea.Quit
-			}
-		}
-		m.textinput, cmd = m.textinput.Update(msg)
 	}
+	m.textinput, cmd = m.textinput.Update(msg)
 
 	return m, cmd
 }
@@ -149,21 +69,8 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) View() string {
 	res := ""
 
-	switch m.state {
-	case int(StateStart):
-		res += "What do you want to log?\n\n"
-		for i, choice := range m.choices {
-			cursor := " "
-			if m.cursor == i {
-				cursor = ">"
-			}
-
-			res += fmt.Sprintf("%s %s\n", cursor, choice)
-		}
-	default:
-		res += fmt.Sprintf("Log a %s\n", stateNames[m.state])
-		res += m.textinput.View()
-	}
+	res += "Log your thoughts\n"
+	res += m.textinput.View()
 
 	res += "\nPress ctrl+c to quit.\n"
 
